c3ddd26a2eece2a7bb558cb67d2f92846f9b8402
Hanno Cornelius <68783915+jm-clius@users.noreply.github.com>
2021-08-24 17:12:19 +0200

    Change TXT record length field to unsigned (0-255)
    This follows from https://www.ietf.org/rfc/rfc6763.txt. Currently any length value larger than 127 would result in `value out of range [RangeError]` exception.

--- src/dnsclientpkg/records/txt.nim
+++ src/dnsclientpkg/records/txt.nim
@@ -1,9 +1,9 @@
 type TXTRecord* = ref object of ResourceRecord
-    length*: int8
+    length*: uint8
     data*: string
 
 method toString*(r: TXTRecord): string = r.data
 
 method parse*(r: TXTRecord, data: StringStream) =
-    r.length = data.readInt8()
-    r.data = data.readStr(r.length)
+    r.length = data.readUint8()
+    r.data = data.readStr(r.length.int)
